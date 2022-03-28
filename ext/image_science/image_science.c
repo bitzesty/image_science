fbbf15086b127572e5951a9df2536de57e755ed1
Nickolay Loshkarev <nickolay.loshkarev@gmail.com>
2022-03-28 16:44:52 +0400

    Reorder method definition

--- ext/image_science/image_science.c
+++ ext/image_science/image_science.c
@@ -231,8 +231,8 @@ extern "C" {
     c = rb_const_get(c, rb_intern("ImageScience"));
 
     rb_define_method(c, "height", (VALUE(*)(ANYARGS))height, 0);
+    rb_define_method(c, "resize", (VALUE(*)(ANYARGS))resize, 3);
     rb_define_method(c, "resize", (VALUE(*)(ANYARGS))_resize, 2);
-    // rb_define_method(c, "resize", (VALUE(*)(ANYARGS))resize, 3);
     rb_define_method(c, "save", (VALUE(*)(ANYARGS))save, 1);
     rb_define_method(c, "width", (VALUE(*)(ANYARGS))width, 0);
     rb_define_method(c, "with_crop", (VALUE(*)(ANYARGS))with_crop, 4);
